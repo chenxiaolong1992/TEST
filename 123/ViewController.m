e6e64b556dd64d11f9ab15e4177d057a673cb6d2
cxlww <952688183@qq.com>
2016-05-26 10:48:54 +0800

    第二次提交

--- 123/ViewController.m
+++ 123/ViewController.m
@@ -18,6 +18,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"你好");
+    
+    NSLog(@"你真好");
 }
 
 - (void)didReceiveMemoryWarning {
