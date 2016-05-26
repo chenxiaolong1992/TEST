82240b418fc1a9e2cdbb1d48198f030925e37881
cxlww <952688183@qq.com>
2016-05-26 10:43:03 +0800

    Initial Commit

---
+++ 123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  123
+//
+//  Created by mac on 16/5/11.
+//  Copyright (c) 2016年 cxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
