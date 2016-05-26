95fab129ac8c0565e2b75617de59e10f970752e1
erbinan <1591494559@qq.com>
2016-05-26 16:08:48 +0800

    jjj

--- 123/ViewController.m
+++ 123/ViewController.m
@@ -1,21 +1,4 @@
-//
-//  ViewController.m
-//  123
-//
-//  Created by mac on 16/5/11.
-//  Copyright (c) 2016年 cxl. All rights reserved.
-//
 
-#import "ViewController.h"
-
-@interface ViewController ()
-
-@end
-
-@implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     //注意：在commit提交的是是从暂停区提交的。在每次commit之前，都要把修改过后的文件进行add的操作，，如果不进行add，在commit的时候，新修改的内容是无法提交的仓库的
     //push的时候，是根据你commit的记录进行push的，每次push之前，要先把修改过后的内容进行commit，直接push，导致新内容无法push到远程仓库。
@@ -54,11 +37,4 @@
     NSLog(@"比一比，龙哥强哥看谁吃屎多！！");
     NSLog(@"又是走过那条昏暗的小巷,那小屋里红色的霓虹灯在照耀,我抽着烟,忽然听到里面传来了一阵骚叫“小帅哥,来玩嘛~” 老子一下子扔开烟头,不屑地笑“这次老子还不骑疯你?”,飞扑过去,投了一个币,熟悉的歌声响起“爸爸的爸爸叫爷爷!爸爸的妈妈叫奶奶!……” 这摇摇乐, 刺激得我头昏目眩的 ");
     
-}
 
-- (void)didReceiveMemoryWarning {
-    
-    // Dispose of any resources that can be recreated.
-}
-
-@end
